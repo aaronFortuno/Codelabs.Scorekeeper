a3e1d3548950dbf15171d2bf4c0e1a5781d50a23
aaronFortuno <aaron.fortuno@protonmail.com>
2023-08-02 07:15:55 +0200

    - functional app btw

--- app/src/main/java/net/estemon/codelabs12_scorekeeper/MainActivity.java
+++ app/src/main/java/net/estemon/codelabs12_scorekeeper/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView mScoreText1;
     private TextView mScoreText2;
 
+    private ImageButton decTeam1;
+    private ImageButton incTeam1;
+    private ImageButton decTeam2;
+    private ImageButton incTeam2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +30,23 @@ public class MainActivity extends AppCompatActivity {
         // Find TextViews by ID
         mScoreText1 = findViewById(R.id.score_1);
         mScoreText2 = findViewById(R.id.score_2);
+
+        // Find ImageViews by ID
+        decTeam1 = findViewById(R.id.decreaseTeam1);
+        incTeam1 = findViewById(R.id.increaseTeam1);
+        decTeam2 = findViewById(R.id.decreaseTeam2);
+        incTeam2 = findViewById(R.id.increaseTeam2);
+
+        // Set onClickListeners to each Imagebutton
+        decTeam1.setOnClickListener(this::decreaseScore);
+        incTeam1.setOnClickListener(this::increaseScore);
+        decTeam2.setOnClickListener(this::decreaseScore);
+        incTeam2.setOnClickListener(this::increaseScore);
+
     }
 
     /**
-     * Method that handles the onClick of both the decrement buttons
+     * Method that handles the behavior of both the decrement buttons
      * @param view The button view that was clicked
      */
     public void decreaseScore(View view) {
@@ -45,17 +64,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Method that handles the onClick of both the increment buttons
+     * Method that handles the behavior of both the increment buttons
      * @param view The button view that was clicked
      */
     public void increaseScore(View view) {
         // Get the ID of the button that was clicked.
         int viewID = view.getId();
-        if (viewID == R.id.decreaseTeam1) { // If it was on Team 1
+        if (viewID == R.id.increaseTeam1) { // If it was on Team 1
             // Increment the score and update the TextView
             mScore1++;
             mScoreText1.setText(String.valueOf(mScore1));
-        } else if (viewID == R.id.decreaseTeam2) { // If it was on Team 2
+        } else if (viewID == R.id.increaseTeam2) { // If it was on Team 2
             // Increment the score and update the TextView
             mScore2++;
             mScoreText2.setText(String.valueOf(mScore2));
