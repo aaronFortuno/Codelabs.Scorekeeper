28d2ad5fa9e06b20aa181a0cd765d118b8a003c5
aaronFortuno <aaron.fortuno@protonmail.com>
2023-08-02 06:59:35 +0200

    Adding methods Rearranging layout code

--- app/src/main/java/net/estemon/codelabs12_scorekeeper/MainActivity.java
+++ app/src/main/java/net/estemon/codelabs12_scorekeeper/MainActivity.java
@@ -3,12 +3,32 @@ package net.estemon.codelabs12_scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Member variables for holding the score
+    private int mScore1;
+    private int mScore2;
+
+    // Member variables for showing the score
+    private TextView mScoreText1;
+    private TextView mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find TextViews by ID
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+    }
+
+    public void decreaseScore(View view) {
+    }
+
+    public void increaseScore(View view) {
     }
 }
