cbe8bacb464605104c56f6bb1c93d4a2ec696169
aaronFortuno <aaron.fortuno@protonmail.com>
2023-08-02 08:35:53 +0200

    - Added landscape qualifier - Resolved ActionBar in night mode

--- app/src/main/java/net/estemon/codelabs12_scorekeeper/MainActivity.java
+++ app/src/main/java/net/estemon/codelabs12_scorekeeper/MainActivity.java
@@ -1,8 +1,12 @@
 package net.estemon.codelabs12_scorekeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -22,9 +26,13 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton decTeam2;
     private ImageButton incTeam2;
 
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         // Find TextViews by ID
@@ -43,6 +51,45 @@ public class MainActivity extends AppCompatActivity {
         decTeam2.setOnClickListener(this::decreaseScore);
         incTeam2.setOnClickListener(this::increaseScore);
 
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            // Set the score text views.
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Check if the correct item was clicked
+        if (item.getItemId() == R.id.night_mode) {
+            // Get the night mode state of the app.
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            // Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Recreate the activity for the theme change to take effect
+            recreate();
+        }
+        return true;
     }
 
     /**
@@ -79,5 +126,13 @@ public class MainActivity extends AppCompatActivity {
             mScore2++;
             mScoreText2.setText(String.valueOf(mScore2));
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the scores
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 }
